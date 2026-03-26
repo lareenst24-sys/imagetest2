cfc92aba1f6cec7cb1afbb58b673373d3bece7b7
lareenst24-sys <lareenst24@gmail.com>
2026-03-27 01:56:35 +0530

    Implement user authentication with Supabase

---
+++ script.js
@@ -0,0 +1,73 @@
+const SUPABASE_URL = "https://zyhiyorqjkvqdnubzynf.supabase.coL";
+const SUPABASE_ANON_KEY = "sb_publishable_lxmie2q8CLR5C5W9OBhGhA_cMnMEfE0";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const loginBox = document.getElementById("login-form");
+const registerBox = document.getElementById("register-form");
+
+const showRegister = document.getElementById("showRegister");
+const showLogin = document.getElementById("showLogin");
+
+showRegister.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginBox.classList.add("hidden");
+  registerBox.classList.remove("hidden");
+});
+
+showLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  registerBox.classList.add("hidden");
+  loginBox.classList.remove("hidden");
+});
+
+function showMessage(message) {
+  alert(message);
+}
+
+document.getElementById("registerForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("registerName").value.trim();
+  const email = document.getElementById("registerEmail").value.trim();
+  const password = document.getElementById("registerPassword").value.trim();
+
+  const { data, error } = await supabaseClient.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        name: name
+      }
+    }
+  });
+
+  if (error) {
+    showMessage("Register error: " + error.message);
+    return;
+  }
+
+  showMessage("Registered successfully. Check your email if confirmation is enabled.");
+  registerBox.classList.add("hidden");
+  loginBox.classList.remove("hidden");
+});
+
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("loginEmail").value.trim();
+  const password = document.getElementById("loginPassword").value.trim();
+
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    showMessage("Login error: " + error.message);
+    return;
+  }
+
+  showMessage("Login successful!");
+  window.location.href = "dashboard.html";
+});
