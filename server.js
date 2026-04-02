ca3993feff4964360c5f5eb3ae95ff6328b6c558
lareenst24-sys <lareenst24@gmail.com>
2026-04-03 04:02:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -153,6 +153,9 @@ app.post("/api/debug-user", async (req, res) => {
     return res.status(500).json({ error: "Server error" });
   }
 });
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
 });
